cca1aa4e2161f14503d4c74f766765528a1d575a
goinesjalen <goinesjalen@gmail.com>
2024-03-24 13:32:09 -0400

    Implement mock testing

--- CourseManagementService/api/tests.py
+++ CourseManagementService/api/tests.py
@@ -1,3 +1,8 @@
 from django.test import TestCase
 
 # Create your tests here.
+
+# class CourseTest(TestCase):
+#     def test_name_here(self):
+#         expected, output = 3, 3
+#         self.assertEqual(expected, output)
